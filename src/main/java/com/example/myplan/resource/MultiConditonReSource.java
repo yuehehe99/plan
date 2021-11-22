0252ad5541a600785a97663821b1b2d387a9318a
hehe.yuethoughtworks.com <hehe.yue@thoughtworks.com>
2021-11-22 15:21:32 +0800

    nothing

--- src/main/java/com/example/myplan/resource/MultiConditonReSource.java
+++ src/main/java/com/example/myplan/resource/MultiConditonReSource.java
@@ -3,17 +3,14 @@ package com.example.myplan.resource;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-
 @Data
 @Builder
 public class MultiConditonReSource {
 
     private Long taskId;
 
-    private Long userId; //多条件查询不能查外键？
+    private Long userId;
 
-    @NotBlank(message = "name can not be blank!")
     private String name;
 
     private String content;
